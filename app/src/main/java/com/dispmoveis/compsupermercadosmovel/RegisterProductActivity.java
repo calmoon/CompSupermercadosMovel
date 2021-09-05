23f3f69f09bf64c7d10402a47d3eafc495405227
EdisonHenrique <edinhobatalha1@gmail.com>
2021-09-05 16:46:21 -0300

    botão de adicionar e subtrair funcionando

--- app/src/main/java/com/dispmoveis/compsupermercadosmovel/RegisterProductActivity.java
+++ app/src/main/java/com/dispmoveis/compsupermercadosmovel/RegisterProductActivity.java
@@ -2,6 +2,10 @@ package com.dispmoveis.compsupermercadosmovel;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterProductActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class RegisterProductActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_product);
+
+        EditText etndPrice = findViewById(R.id.etndPrice);
+        EditText etnAmount = findViewById(R.id.etnAmount);
+        etnAmount.setText("1");
+
+        Button btnAdd = findViewById(R.id.btnAdd);
+        Button btnSubtract = findViewById(R.id.btnSubtract);
+
+        TextView tvProductTotal = findViewById(R.id.tvProductTotal);
+        TextView tvCartTotal = findViewById(R.id.tvCartTotal);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer add = Integer.parseInt(etnAmount.getText().toString()) + 1;
+                etnAmount.setText(add.toString());
+            }
+        });
+        btnSubtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer subtract = Integer.parseInt(etnAmount.getText().toString()) - 1;
+                if (subtract > 0) {
+                    etnAmount.setText(subtract.toString());
+                }
+            }
+        });
     }
 }
