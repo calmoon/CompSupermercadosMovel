ff03907333891c75786a34078405897e9b0d3029
EdisonHenrique <edinhobatalha1@gmail.com>
2021-09-06 10:25:56 -0300

    criação da toolbar com botão de sair funcionando

--- app/src/main/java/com/dispmoveis/compsupermercadosmovel/HomeActivity.java
+++ app/src/main/java/com/dispmoveis/compsupermercadosmovel/HomeActivity.java
@@ -20,9 +20,29 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        Toolbar toolbar = findViewById(R.id.toolbar_home);
+        setSupportActionBar(toolbar);
+
         String login = Config.getLogin(HomeActivity.this);
 
         TextView tvWebData = findViewById(R.id.tvWebData);
         tvWebData.setText("Olá " + login);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_toolbar, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        Config.setLogin(HomeActivity.this, "");
+        Config.setPassword(HomeActivity.this, "");
+        Intent i = new Intent(HomeActivity.this, LoginActivity.class);
+        startActivity(i);
+        return true;
     }
 }
