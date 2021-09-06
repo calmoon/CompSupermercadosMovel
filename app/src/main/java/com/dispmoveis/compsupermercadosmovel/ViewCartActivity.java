c8c2c2534faa208ef2d009ed128efcd44b17ca3d
calmoon <jvitor210@hotmail.com>
2021-09-06 20:34:40 -0300

    implementado o widget nambicompany/expandable-fab

---
+++ app/src/main/java/com/dispmoveis/compsupermercadosmovel/ViewCartActivity.java
@@ -0,0 +1,26 @@
+package com.dispmoveis.compsupermercadosmovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.dispmoveis.compsupermercadosmovel.databinding.ActivityCreateAccountBinding;
+import com.dispmoveis.compsupermercadosmovel.databinding.ActivityViewCartBinding;
+
+public class ViewCartActivity extends AppCompatActivity {
+
+    private ActivityViewCartBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityViewCartBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+    }
+
+
+}
