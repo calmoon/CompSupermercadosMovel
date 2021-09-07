c827ae3f02d9306be22aab6551cc210e6e027439
EdisonHenrique <edinhobatalha1@gmail.com>
2021-09-07 13:19:50 -0300

    classe view holder para usar em adapters

---
+++ app/src/main/java/com/dispmoveis/compsupermercadosmovel/ViewHolder.java
@@ -0,0 +1,11 @@
+package com.dispmoveis.compsupermercadosmovel;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    public ViewHolder(View view) {
+        super(view);
+    }
+}
