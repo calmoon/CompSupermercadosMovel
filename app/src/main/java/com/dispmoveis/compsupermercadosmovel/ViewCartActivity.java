c45b24a983f576e04fa99c4b9d992201efc0c57d
EdisonHenrique <edinhobatalha1@gmail.com>
2021-09-09 01:29:10 -0300

    montada a lógica para disparar a camera e depois passar para RegisterProductActivity e inserir no RecyclerView o preview do produto

--- app/src/main/java/com/dispmoveis/compsupermercadosmovel/ViewCartActivity.java
+++ app/src/main/java/com/dispmoveis/compsupermercadosmovel/ViewCartActivity.java
@@ -4,24 +4,34 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 
 import com.dispmoveis.compsupermercadosmovel.databinding.ActivityViewCartBinding;
 
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ViewCartActivity extends AppCompatActivity {
 
-    static int NEW_ITEM_REQUEST = 1;
+    static DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
+    static int NEW_BARCODE_REQUEST = 1;
+    static int NEW_ITEM_REQUEST = 2;
 
     private ActivityViewCartBinding binding;
 
     private List<CartItemData> cartItems = new ArrayList<>();
     private CartAdapter cartAdapter = new CartAdapter(cartItems);
+
+    private Double total = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,10 @@ public class ViewCartActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        String cartName = "Seu carrinho #" + getIntent().getStringExtra("cartHistoryItemsSize");
+
+        binding.editTextTextPersonName.setText(cartName);
+        binding.textTotal.setText("0.00");
         binding.recyclerCart.setLayoutManager(new LinearLayoutManager(this));
         binding.recyclerCart.setAdapter(cartAdapter);
         binding.recyclerCart.setHasFixedSize(true);
@@ -37,36 +51,60 @@ public class ViewCartActivity extends AppCompatActivity {
         binding.optionBarcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
-                //Intent i = new Intent( );
-                //ActivityResultContracts.StartActivityForResult( );
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(i, NEW_BARCODE_REQUEST);
             }
         });
 
         binding.optionCatalog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO
-                //Intent i = new Intent( );
-                //ActivityResultContracts.StartActivityForResult( );
             }
         });
 
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                Integer cardSize = cartItems.size() + 1;
+                String date = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
+                i.putExtra("cardName", binding.editTextTextPersonName.getText().toString());
+                i.putExtra("cardTotal", decimalFormat.format(total));
+                i.putExtra("cardSize", cardSize + " produtos");
+                i.putExtra("cardDate", "Última modificação: " + date);
+                setResult(Activity.RESULT_OK, i);
+                finish();
+            }
+        });
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == NEW_ITEM_REQUEST) {
-            Uri productImageUri = data.getData();
-            String productName = data.getStringExtra("productName");
-            double productPrice = data.getDoubleExtra("productPrice", 0);
-            Integer productQty = data.getIntExtra("productQty", 1);
+        if (resultCode == Activity.RESULT_OK) {
 
-            CartItemData newItem = new CartItemData(productImageUri, productName, productPrice, productQty);
-            cartItems.add(newItem);
+            if (requestCode == NEW_BARCODE_REQUEST) {
+                Intent i = new Intent(ViewCartActivity.this, RegisterProductActivity.class);
+                i.putExtra("total", total);
+                startActivityForResult(i, NEW_ITEM_REQUEST);
+            }
 
-            cartAdapter.notifyItemInserted(cartItems.size()-1);
+            if (requestCode == NEW_ITEM_REQUEST) {
+                //Uri productImageUri = data.getData();
+                String productName = data.getStringExtra("productName");
+                Double productPrice = data.getDoubleExtra("productPrice", 0);
+                Integer productQty = data.getIntExtra("productQty", 1);
+                Double total = data.getDoubleExtra("total", 0);
+
+                this.total = total;
+                binding.textTotal.setText(decimalFormat.format(total));
+
+                CartItemData newItem = new CartItemData(productName, productPrice, productQty);
+                cartItems.add(newItem);
+
+                cartAdapter.notifyItemInserted(cartItems.size()-1);
+            }
+
         }
     }
 
