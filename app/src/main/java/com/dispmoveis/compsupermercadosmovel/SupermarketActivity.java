db68369f83accb4010a935867e018c64ff6cb5d3
EdisonHenrique <edinhobatalha1@gmail.com>
2021-09-09 01:40:46 -0300

    Activity criada para pegar o supermercado onde o usuario se encontra

---
+++ app/src/main/java/com/dispmoveis/compsupermercadosmovel/SupermarketActivity.java
@@ -0,0 +1,33 @@
+package com.dispmoveis.compsupermercadosmovel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.dispmoveis.compsupermercadosmovel.databinding.ActivityHomeBinding;
+import com.dispmoveis.compsupermercadosmovel.databinding.ActivitySupermarketBinding;
+
+public class SupermarketActivity extends AppCompatActivity {
+
+    private ActivitySupermarketBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySupermarketBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                setResult(Activity.RESULT_OK, i);
+                finish();
+            }
+        });
+    }
+}
