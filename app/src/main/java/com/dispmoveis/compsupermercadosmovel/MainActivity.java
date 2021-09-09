c5b1de94f703dff2d4e9056b2afb4248115ac173
EdisonHenrique <edinhobatalha1@gmail.com>
2021-09-09 01:24:43 -0300

    criado na MainActivity uma requisição de permissção de camera, com adaptação para a versão nova do android

--- app/src/main/java/com/dispmoveis/compsupermercadosmovel/MainActivity.java
+++ app/src/main/java/com/dispmoveis/compsupermercadosmovel/MainActivity.java
@@ -1,16 +1,35 @@
 package com.dispmoveis.compsupermercadosmovel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    static int RESULT_REQUEST_PERMISSION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.CAMERA);
+
+        checkForPermissions(permissions);
+    }
+
+    private void startApp(){
         if(Config.getLogin(MainActivity.this).isEmpty()) {
             Intent i = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(i);
@@ -23,4 +42,71 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void checkForPermissions(List<String> permissions) {
+        List<String> permissionsNotGranted = new ArrayList<>();
+        for (String permission : permissions){
+            if (!hasPermission(permission)){
+                permissionsNotGranted.add(permission);
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (permissionsNotGranted.size() > 0){
+                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION);
+            } else {
+                startApp();
+            }
+        }
+    }
+
+    private boolean hasPermission(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        List<String> permissionsRejected = new ArrayList<>();
+        if (requestCode == RESULT_REQUEST_PERMISSION) {
+            for (String permission : permissions){
+                if (!hasPermission(permission)) {
+                    permissionsRejected.add(permission);
+                }
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (permissionsRejected.size() > 0) {
+                String alertMesssage = "Para usar esse app, é preciso conceder a permissão de câmera.";
+                String alertOk = "Ok";
+                boolean showRationale = shouldShowRequestPermissionRationale(permissionsRejected.get(0));
+                if (showRationale) {
+                    new AlertDialog.Builder(MainActivity.this).
+                            setMessage(alertMesssage).
+                            setPositiveButton(alertOk, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
+                                }
+                            }).create().show();
+                } else {
+                    new AlertDialog.Builder(MainActivity.this).
+                            setMessage(alertMesssage).
+                            setPositiveButton(alertOk, new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ActivityCompat.finishAffinity(MainActivity.this);
+                                }
+                            }).create().show();
+
+                }
+            }
+            else {
+                startApp();
+            }
+        }
+    }
 }
