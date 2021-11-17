d2e3f7c1c3751c9b3c737b7d3b0f7b9312f11c89
calmoon <jvitor210@hotmail.com>
2021-11-17 19:08:02 -0300

    lógica preliminar de carreg. do nome do supermercado

--- app/src/main/java/com/dispmoveis/compsupermercadosmovel/ui/productsearch/ProductSearchActivity.java
+++ app/src/main/java/com/dispmoveis/compsupermercadosmovel/ui/productsearch/ProductSearchActivity.java
@@ -5,21 +5,38 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.GridLayoutManager;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.dispmoveis.compsupermercadosmovel.R;
 import com.dispmoveis.compsupermercadosmovel.model.SupermarketItem;
 import com.dispmoveis.compsupermercadosmovel.databinding.ActivityProductSearchBinding;
+import com.dispmoveis.compsupermercadosmovel.util.Config;
+import com.dispmoveis.compsupermercadosmovel.util.HttpRequest;
 import com.dispmoveis.compsupermercadosmovel.util.Util;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
 
 public class ProductSearchActivity extends AppCompatActivity {
+
+    private String supermarketId;
 
     private ProductSearchViewModel productSearchViewModel;
 
     private ActivityProductSearchBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +44,16 @@ public class ProductSearchActivity extends AppCompatActivity {
         binding = ActivityProductSearchBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+
+        //TODO: usar o código comentado quando quando não for mais
+        //      necessário executar a activity de forma isolada
+        /*
+        Intent i = getIntent();
+        this.supermarketId = i.getStringExtra("supermarketId");
+        */
+        this.supermarketId = "1";
+
+        loadSupermarketName();
 
         productSearchViewModel = new ViewModelProvider(this)
                 .get(ProductSearchViewModel.class);
@@ -48,4 +75,43 @@ public class ProductSearchActivity extends AppCompatActivity {
 
         binding.recyclerProductSearch.setHasFixedSize(true);
     }
+
+    private void loadSupermarketName() {
+        Executors.newSingleThreadExecutor().execute(new Runnable() {
+            @Override
+            public void run() {
+
+                HttpRequest httpRequest = new HttpRequest(
+                        // TODO: colocar a url da api no config
+                        Config.SERVER_URL_BASE + "server_select.php",
+                        "GET",
+                        "UTF-8"
+                );
+                httpRequest.addParam("queryType", "supermarketInfo");
+                httpRequest.addParam("id", supermarketId);
+
+                try {
+                    InputStream inputStream = httpRequest.execute();
+                    String resultString = Util.inputStream2String(inputStream, "UTF-8");
+                    JSONObject responseJSON = new JSONObject(resultString);
+                    httpRequest.finish();
+
+                    int resultCode = responseJSON.getInt("result_code");
+
+                    if (resultCode == 1) {
+                        String supermarketName = responseJSON.getJSONArray("result")
+                                .getJSONObject(0)
+                                .getString("nome");
+                        binding.textSelectedSupermarket.setText(supermarketName);
+                    }
+                }
+
+                catch (IOException | JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
 }
