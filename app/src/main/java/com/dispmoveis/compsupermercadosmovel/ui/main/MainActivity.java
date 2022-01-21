1f37eb8c07a27fc45cc36be59ce64e27b0e383bb
EdisonHenrique <edinhobatalha1@gmail.com>
2022-01-21 12:31:58 -0300

    Retirado o pedido de permissão antigo da camera que estava incorreto

--- app/src/main/java/com/dispmoveis/compsupermercadosmovel/ui/main/MainActivity.java
+++ app/src/main/java/com/dispmoveis/compsupermercadosmovel/ui/main/MainActivity.java
@@ -27,10 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        List<String> permissions = new ArrayList<>();
-        permissions.add(Manifest.permission.CAMERA);
-
-        checkForPermissions(permissions);
+        startApp();
     }
 
     private void startApp(){
@@ -43,74 +40,6 @@ public class MainActivity extends AppCompatActivity {
             Intent i = new Intent(MainActivity.this, PreviousCartsActivity.class);
             startActivity(i);
             finish();
-        }
-    }
-
-    private void checkForPermissions(List<String> permissions) {
-        List<String> permissionsNotGranted = new ArrayList<>();
-        for (String permission : permissions){
-            if (!hasPermission(permission)){
-                permissionsNotGranted.add(permission);
-            }
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            if (permissionsNotGranted.size() > 0){
-                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION);
-            } else {
-                startApp();
-            }
-        }
-    }
-
-    private boolean hasPermission(String permission) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
-        }
-        return false;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        List<String> permissionsRejected = new ArrayList<>();
-        if (requestCode == RESULT_REQUEST_PERMISSION) {
-            for (String permission : permissions){
-                if (!hasPermission(permission)) {
-                    permissionsRejected.add(permission);
-                }
-            }
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (permissionsRejected.size() > 0) {
-                String alertMesssage = "Para usar esse app, é preciso conceder a permissão de câmera.";
-                String alertOk = "Ok";
-                boolean showRationale = shouldShowRequestPermissionRationale(permissionsRejected.get(0));
-                if (showRationale) {
-                    new AlertDialog.Builder(MainActivity.this).
-                            setMessage(alertMesssage).
-                            setPositiveButton(alertOk, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
-                                }
-                            }).create().show();
-                } else {
-                    new AlertDialog.Builder(MainActivity.this).
-                            setMessage(alertMesssage).
-                            setPositiveButton(alertOk, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    ActivityCompat.finishAffinity(MainActivity.this);
-                                }
-                            }).create().show();
-
-                }
-            }
-            else {
-                startApp();
-            }
         }
     }
 }
